ea23b8a4f25f1c57fcb469bd9a016bec9bb49cd9
philduonggit <phil.duong@polyvietnam.edu.vn>
2023-04-05 14:52:04 +0700

    indexall

--- index.js
+++ index.js
@@ -6,6 +6,28 @@ const app = express();
     console.log("Just got a request!")
     res.send('Yo!')
 })*/
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // // Website you wish to allow to connect
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // // Request methods you wish to allow
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // // Request headers you wish to allow
+    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // // Set to true if you need the website to include cookies in the requests sent
+    // // to the API (e.g. in case you use sessions)
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+	
+	res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    // Pass to next layer of middleware
+    next();
+});
 app.get('/arBook/:id', async (req, res) => {
 	//console.log("productURLs", getData());
 	//res.cookie('BFUserType', 'Librarian');
